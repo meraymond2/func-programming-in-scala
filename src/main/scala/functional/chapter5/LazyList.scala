37d987f5798376f51b12dc6389d23ca24e8111fe
Michael Raymond <raymond.michael@gmail.com>
2018-08-11 17:20:21 +0100

    Do exercises up to 5-12

--- src/main/scala/functional/chapter5/LazyList.scala
+++ src/main/scala/functional/chapter5/LazyList.scala
@@ -1,19 +1,101 @@
 package functional.chapter5
 
-// The chapter is describing Streams.
+/* The chapter is describing Streams. */
 trait LazyList[+A] {
   def uncons: Option[(A, LazyList[A])]
 
   def isEmpty: Boolean = uncons.isEmpty
 
-  // Exercise 1
+  /* Exercise 1 */
   def toList: List[A] = this.uncons match {
     case Some((x, xs)) => x :: xs.toList
     case None          => Nil
   }
 
-  // Exercise 2
-  def take(n: Int): LazyList[A] = this // do later
+  /* Exercise 2 */
+  def take(n: Int): LazyList[A] = {
+    @annotation.tailrec
+    def loop(from: LazyList[A], to: List[A], i: Int): LazyList[A] = from.uncons match {
+      case _ if i == n =>
+        LazyList(to:_*)
+      case None =>
+        LazyList(to:_*)
+      case Some((head, tail)) =>
+        loop(tail, to :+ head, i + 1)
+    }
+    loop(this, List.empty[A], 0)
+  }
+
+  /*
+  These confused me a bit, because there is no way to append to the stream. For these, I think
+  it's ok to use a normal List for the return, because they should always be finite, even if the
+  source is infinite.
+
+  Is it possible to append without iterating through the stream?
+  */
+
+  /* Exercise 3 */
+  def takeWhile(p: A => Boolean): LazyList[A] = {
+    @annotation.tailrec
+    def loop(from: LazyList[A], to: List[A]): LazyList[A] = from.uncons match {
+      case Some((head, tail)) if p(head) =>
+        loop(tail, to :+ head)
+      case _ =>
+        LazyList(to:_*)
+    }
+    loop(this, List.empty[A])
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    uncons match {
+      case Some((h, t)) => f(h, t.foldRight(z)(f))
+      case None => z
+    }
+
+  /*
+  So f's second parameter, b in the following, is only evaluated
+  if you use it. And if it's not evaluated, it doesn't continue the loop,
+  because the next iteration is only happening in b's evaluation.
+  :headache:
+   */
+
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a) || b)
+
+  /* Exercise 4 */
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a, b) => p(a) && b)
+
+  /* Exercise 5 */
+  def takeWhile2(p: A => Boolean): LazyList[A] =
+    foldRight(LazyList.empty[A])((a, b) =>
+      if (p(a)) LazyList.cons(a, b) else LazyList.empty[A]
+    )
+
+  /* Exercise 6a */
+  def map[B](f: A => B): LazyList[B] =
+    foldRight(LazyList.empty[B])((a, b) =>
+      LazyList.cons(f(a), b)
+    )
+
+  /* Exercise 6b */
+  def filter(p: A => Boolean): LazyList[A] =
+    foldRight(LazyList.empty[A])((a, b) =>
+      if (p(a)) LazyList.cons(a, b) else b
+    )
+
+  /* Exercise 6c */
+  def append[B >: A](x: B): LazyList[B] =
+    foldRight(LazyList(x))((a, b) => LazyList.cons(a, b))
+
+  /* Exercise 6d */
+  def flatMap[B](f: A => LazyList[B]): LazyList[B] =
+    map(f).foldRight(LazyList.empty[B])((a, b) =>
+      a.foldRight(b)((aa, bb) =>
+        LazyList.cons(aa, bb)
+      )
+    )
+
 }
 
 object LazyList {
@@ -21,13 +103,55 @@ object LazyList {
   def empty[A]: LazyList[A] =
     new LazyList[A] { def uncons = None }
 
-  def cons[A](hd: => A, tl: => LazyList[A]): LazyList[A] =
+  def cons[A](head: => A, tail: => LazyList[A]): LazyList[A] =
     new LazyList[A] {
-      lazy val uncons = Some(hd, tl)
+      lazy val uncons = Some(head, tail)
     }
 
   def apply[A](as: A*): LazyList[A] =
     if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
+
+  /* Exercise 7 */
+  def constant[A](a: A): LazyList[A] =
+    cons(a, constant(a))
+
+  /* Exercise 8 */
+  def from(n: Int): LazyList[Int] =
+    cons(n, from(n + 1))
+
+  /* Exercise 9 */
+  def fibs: LazyList[Int] = {
+    def loop(a: Int, b: Int): LazyList[Int] =
+      cons(a + b, loop(b, a + b))
+    cons(0, cons(1, loop(0, 1)))
+  }
+
+  /* Exercise 10 */
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): LazyList[A] =
+    f(z) match {
+      case Some((a, b)) =>
+        cons(a, unfold(b)(f))
+      case None => LazyList.empty[A]
+    }
+
+  /* Exercise 11 */
+  def fibs2: LazyList[Int] =
+    cons(0, cons(1, unfold((0, 1))(i => {
+      val (a, b) = i
+      Some(a + b, (b, a + b))
+    })))
+
+  def from2(from: Int): LazyList[Int] =
+    unfold(from)(i => Some(i, i + 1))
+
+  def constant2[A](a: A): LazyList[A] =
+    unfold(a)(_ => Some(a, a))
+
+  def twos: LazyList[Int] =
+    unfold(2)(_ => Some(2, 2))
+
+  /* Exercise 12 */
+  // TODO
 
 }
--- src/test/scala/functional/chapter5/StreamSpec.scala
+++ src/test/scala/functional/chapter5/StreamSpec.scala
@@ -12,4 +12,95 @@ class StreamSpec extends FlatSpec with Matchers {
     empty.toList shouldEqual Nil
   }
 
+  "take" should "return a new lazylist with n number of elements" in {
+    val ll = LazyList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    ll.take(3).toList shouldEqual LazyList(1, 2, 3).toList
+
+    ll.take(15).toList shouldEqual ll.toList
+
+    ll.take(0).toList shouldEqual List.empty[Int]
+  }
+
+  "takeWhile" should "take elements until an element fails the predicate" in {
+    val ll = LazyList("string", "object", "stream", "int", "double", "option")
+
+    ll.takeWhile(_.length > 3).toList shouldEqual List("string", "object", "stream")
+  }
+
+  "forall" should "check whether every element in a stream satifies a predicate" in {
+    val ll = LazyList(88, 30, 99, 72)
+
+    ll.forAll(_ > 50) shouldBe false
+    ll.forAll(_ > 25) shouldBe true
+  }
+
+  "takeWhile2" should "take elements until an element fails the predicate" in {
+    val ll = LazyList("string", "object", "stream", "int", "double", "option")
+
+    ll.takeWhile2(_.length > 3).toList shouldEqual List("string", "object", "stream")
+  }
+
+  "map" should "return a new stream with a function applied to eac element" in {
+    val ll = LazyList(1, 2, 3, 4, 5)
+
+    ll.map(_ * 10).toList shouldEqual List(10, 20, 30, 40, 50)
+  }
+
+  "filter" should "return a stream without any elements that fail the predicate" in {
+    val ll = LazyList("string", "object", "stream", "int", "double", "option")
+
+    ll.filter(_.length > 3).toList shouldEqual List("string", "object", "stream", "double", "option")
+  }
+
+  "append" should "attach an element to the end of the stream" in {
+    val ll = LazyList(1, 2, 3)
+
+    ll.append(4).toList shouldEqual List(1, 2, 3, 4)
+  }
+
+  "flatMap" should "return a new stream with a function applied to each element, and flattened" in {
+    val ll = LazyList("Cascat", "Luna", "Sherlock")
+
+    ll.flatMap(str => LazyList(str.toCharArray:_*)).toList shouldEqual List('C', 'a', 's', 'c', 'a', 't', 'L', 'u', 'n', 'a', 'S', 'h', 'e', 'r', 'l', 'o', 'c', 'k')
+  }
+
+  "constant" should "return an infinite stream of a given value" in {
+    val infinite = LazyList.constant("Cascat")
+
+    infinite.take(3).toList shouldEqual List("Cascat", "Cascat", "Cascat")
+  }
+
+  "from" should "retrn an infinite series of integers, starting a n" in {
+    val infinite = LazyList.from(5)
+
+    infinite.take(5).toList shouldEqual List(5, 6, 7, 8, 9)
+  }
+
+  "fibs" should "return an infinite stream of Fibonacci numbers" in {
+    val fibs = LazyList.fibs
+
+    fibs.take(10).toList shouldEqual List(0, 1, 1, 2, 3, 5, 8, 13, 21, 34)
+  }
+
+  "unfold" should "create a stream from a start point and a function" in {
+    val ll = LazyList.unfold(0)(i => {
+      if (i <= 10) Some(i, i + 2) else None
+    })
+
+    ll.toList shouldEqual List(0, 2, 4, 6, 8, 10)
+
+    val fibs = LazyList.fibs2
+    fibs.take(10).toList shouldEqual List(0, 1, 1, 2, 3, 5, 8, 13, 21, 34)
+
+    val from = LazyList.from2(5)
+    from.take(5).toList shouldEqual List(5, 6, 7, 8, 9)
+
+    val constants = LazyList.constant2("Cascat")
+    constants.take(3).toList shouldEqual List("Cascat", "Cascat", "Cascat")
+
+    val twos = LazyList.twos
+    twos.take(12).toList shouldEqual List(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2)
+  }
+
 }
