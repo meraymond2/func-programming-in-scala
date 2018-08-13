3344062ec2b47f24a31b6fbc5efa8606bf9bb1f4
Michael Raymond <raymond.michael@gmail.com>
2018-08-13 19:16:37 +0100

    Replace class with trait + object

--- src/main/scala/functional/Chapter6/RNG.scala
+++ src/main/scala/functional/Chapter6/RNG.scala
@@ -1,46 +1,53 @@
 package functional.Chapter6
 
-// Changing this to a class, his example with an object doesn't compile
-class RNG(seed: Long) {
+trait RNG {
+  def nextInt: (Int, RNG)
 
-  def nextInt: (Int, RNG) = {
-    val seed2 = (seed*0x5DEECE66DL + 0xBL) & ((1L << 48) - 1)
-    ((seed2 >>> 16).asInstanceOf[Int], new RNG(seed2))
+}
+
+object RNG {
+  def simple(seed: Long): RNG = new RNG {
+    def nextInt: (Int, RNG) = {
+      val seed2 = (seed*0x5DEECE66DL + 0xBL) &
+        ((1L << 48) - 1)
+      ((seed2 >>> 16).asInstanceOf[Int],
+        simple(seed2))
+    }
   }
 
   /* Exercise 1 */
-  def positiveInt: (Int, RNG) = {
-    val (int, rng) = nextInt
+  def positiveInt(rng: RNG): (Int, RNG) = {
+    val (int, rng2) = rng.nextInt
     val positive = if (int == Integer.MIN_VALUE) (int + 1).abs else int.abs
-    (positive, rng)
+    (positive, rng2)
   }
 
   /* Exercise 2 */
-  def double: (Double, RNG) = {
-    val (int, rng) = positiveInt
+  def double(rng: RNG): (Double, RNG) = {
+    val (int, rng2) = RNG.positiveInt(rng)
     val double = int / Integer.MAX_VALUE.toDouble
-    (double, rng)
+    (double, rng2)
   }
 
   /* Exercise 3 */
-  def intDouble: ((Int, Double), RNG) = {
-    val (int, rng2) = positiveInt
-    val (double, rng3) = rng2.double
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (int, rng2) = RNG.positiveInt(rng)
+    val (double, rng3) = RNG.double(rng2)
     ((int, double), rng3)
   }
 
   // skipping doubleInt and double3, cause they're the same thing
 
   /* Exercise 4 */
-  def ints(count: Int): (List[Int], RNG) = {
-    def loop(rng: RNG, list: List[Int], i: Int): (List[Int], RNG) = {
+  def ints(rng: RNG, count: Int): (List[Int], RNG) = {
+    def loop(rng1: RNG, list: List[Int], i: Int): (List[Int], RNG) = {
       if (i < count) {
-        val (int, rng2) = rng.nextInt
+        val (int, rng2) = rng1.nextInt
         loop(rng2, int +: list, i + 1)
       }
-      else (list, rng)
+      else (list, rng1)
     }
-    loop(this, List.empty[Int], 0)
+    loop(rng, List.empty[Int], 0)
   }
 
 }
--- src/test/scala/functional/Chapter6/RNGSpec.scala
+++ src/test/scala/functional/Chapter6/RNGSpec.scala
@@ -3,20 +3,20 @@ import org.scalatest._
 
 class RNGSpec extends FlatSpec with Matchers {
 
-  val seed = 12345678
+  val seed = 12345678L
 
   /* Exercise 1 */
   "positive Int" should "return a pseudo-random positive integer" in {
 
-    val rng = new RNG(seed)
+    val rng = RNG.simple(seed)
 
-    val (int, rng2) = rng.positiveInt
-    val (int2, _) = rng.positiveInt
+    val (int, rng2) = RNG.positiveInt(rng)
+    val (int2, _) = RNG.positiveInt(rng)
 
     int shouldEqual 247797879
     int2 shouldEqual 247797879
 
-    val (int3, _) = rng2.positiveInt
+    val (int3, _) = RNG.positiveInt(rng2)
 
     int3 should not equal 247797879
 
@@ -24,10 +24,10 @@ class RNGSpec extends FlatSpec with Matchers {
 
   /* Exercise 2 */
   "double" should "return a pseudo-random number between 0 and 1" in {
-    val rng = new RNG(seed)
+    val rng = RNG.simple(seed)
 
-    val (double, rng2) = rng.double
-    val (double2, _) = rng2.double
+    val (double, rng2) = RNG.double(rng)
+    val (double2, _) = RNG.double(rng2)
 
     double shouldEqual 0.11538987938100001d
     double2 shouldEqual 0.25709313259324673d
@@ -36,9 +36,9 @@ class RNGSpec extends FlatSpec with Matchers {
 
   /* Exercise 3 */
   "intDouble" should "return a tuple with an int and a double" in {
-    val rng = new RNG(seed)
+    val rng = RNG.simple(seed)
 
-    val ((int, double), _) = rng.intDouble
+    val ((int, double), _) = RNG.intDouble(rng)
 
     int shouldEqual 247797879
     double shouldEqual 0.25709313259324673d
@@ -46,10 +46,10 @@ class RNGSpec extends FlatSpec with Matchers {
 
   /* Exercise 4 */
   "ints" should "return a list of length n of psuedo-random integers" in {
-    val rng = new RNG(seed)
+    val rng = RNG.simple(seed)
 
-    val (ints, rng2) = rng.ints(5)
-    val (ints2, _) = rng.ints(6)
+    val (ints, rng2) = RNG.ints(rng, 5)
+    val (ints2, _) = RNG.ints(rng, 6)
 
     ints shouldEqual List(149386600, 1840538571, -2068089078, -552103298, -247797879)
     ints2 shouldEqual List(-2063692454, 149386600, 1840538571, -2068089078, -552103298, -247797879)
